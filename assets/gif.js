a2a0207948ddd7e1b9f6b3b174c1ae38f3367e4c
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-04-02 19:37:31 -0500

    Add array to topics variable

---
+++ assets/gif.js
@@ -0,0 +1,2 @@
+// Creating an array for topics var
+var topics = ["tom segura", "joe rogan", "mindy kaling", "hannibal buress", "tiffany haddish", "jordan peele", "issa rae", "dave chappelle", "chris rock", "bill burr"]
