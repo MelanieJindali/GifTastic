d04423e20ca66b95bc012d2b404b2a73246687de
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-04-03 13:04:55 -0500

    Display buttons to HTML page

--- assets/gif.js
+++ assets/gif.js
@@ -1,2 +1,21 @@
+$(document).ready(function() {
+
 // Creating an array for topics var
-var topics = ["tom segura", "joe rogan", "mindy kaling", "hannibal buress", "tiffany haddish", "jordan peele", "issa rae", "dave chappelle", "chris rock", "bill burr"]
+var funnyPpl = ["tom segura", "joe rogan", "mindy kaling", "hannibal buress", "tiffany haddish", "jordan peele", "issa rae", "dave chappelle", "chris rock", "bill burr"];
+
+// Displaying buttons
+function renderBtns() {
+    $("#btns").empty();
+
+    // Generate buttons for each person in array
+    for (var i = 0; i < funnyPpl.length; i++) {
+        var topBtns = $("<button>");
+        topBtns.addClass("people");
+        topBtns.attr("data-name", funnyPpl[i]);
+        topBtns.text(funnyPpl[i]);
+        $("#btns").append(topBtns);
+    }
+}
+renderBtns();
+
+});
